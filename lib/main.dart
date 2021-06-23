c43986b2ed5ce1aa7af746c0a8b32d3dc355b65b
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-06-22 22:34:55 -0300

    social login button + login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }

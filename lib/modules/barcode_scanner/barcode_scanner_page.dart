b08c9d1e00c336fe071c78b5bce27b9969a9678c
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-06-25 22:58:17 -0300

    scanner controller + status + page + static mlkit models

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_controller.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_status.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
-import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
-import 'package:payflow/shared/widgets/label_button/label_button.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
@@ -14,61 +14,114 @@ class BarcodeScannerPage extends StatefulWidget {
 }
 
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
+  final controller = BarcodeScannerController();
+
+  @override
+  void initState() {
+    controller.getAvailableCameras();
+    controller.statusNotifier.addListener(() {
+      if (controller.status.hasBarcode) {
+        Navigator.pushReplacementNamed(
+          context,
+          '/insert_boleto',
+          arguments: controller.status.barcode,
+        );
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return BottomSheetWidget(
-    //   title: 'Não foi possível identificar um código de barras.',
-    //   subTitle: 'Tente escanear novamente ou digite o código do seu boleto.',
-    //   primaryLabel: 'Escanear novamente',
-    //   secondaryLabel: 'Digitar código',
-    //   primaryCb: () {},
-    //   secondaryCb: () {},
-    // );
     return SafeArea(
       top: true,
       bottom: true,
       left: true,
       right: true,
-      child: RotatedBox(
-        quarterTurns: 1,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Text(
-              'Escaneie o código de barras do boleto',
-              style: TextStyles.buttonBackground,
-            ),
-            leading: BackButton(
-              color: AppColors.background,
-            ),
+      child: Stack(
+        children: [
+          ValueListenableBuilder<BarcodeScannerStatus>(
+            valueListenable: controller.statusNotifier,
+            builder: (_, status, __) {
+              if (status.showCamera) {
+                return Container(
+                  color: Colors.blue,
+                  child: controller.cameraController!.buildPreview(),
+                );
+              } else {
+                return Container();
+              }
+            },
           ),
-          body: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  color: Colors.black.withOpacity(0.6),
+          RotatedBox(
+            quarterTurns: 1,
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Colors.black,
+                title: Text(
+                  'Escaneie o código de barras do boleto',
+                  style: TextStyles.buttonBackground,
+                ),
+                leading: BackButton(
+                  color: AppColors.background,
                 ),
               ),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  color: Colors.transparent,
-                ),
+              body: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.black.withOpacity(0.6),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.black.withOpacity(0.6),
+                    ),
+                  )
+                ],
               ),
-              Expanded(
-                child: Container(
-                  color: Colors.black.withOpacity(0.6),
-                ),
-              )
-            ],
+              bottomNavigationBar: SetLabelButtons(
+                primaryLabel: 'Inserir código do boleto',
+                primaryCb: () {},
+                secondaryLabel: 'Adicionar da galeria',
+                secondaryCb: () {},
+              ),
+            ),
           ),
-          bottomNavigationBar: SetLabelButtons(
-            primaryLabel: 'Inserir código do boleto',
-            primaryCb: () {},
-            secondaryLabel: 'Adicionar da galeria',
-            secondaryCb: () {},
-          ),
-        ),
+          ValueListenableBuilder<BarcodeScannerStatus>(
+              valueListenable: controller.statusNotifier,
+              builder: (_, status, __) {
+                if (status.hasError) {
+                  return Align(
+                      alignment: Alignment.bottomLeft,
+                      child: BottomSheetWidget(
+                        title: 'Não foi possível identificar um código de barras.',
+                        subTitle: 'Tente escanear novamente ou digite o código do seu boleto.',
+                        primaryLabel: 'Escanear novamente',
+                        secondaryLabel: 'Digitar código',
+                        primaryCb: () {
+                          controller.scanWithCamera();
+                        },
+                        secondaryCb: () {},
+                      ));
+                } else {
+                  return Container();
+                }
+              })
+        ],
       ),
     );
   }
